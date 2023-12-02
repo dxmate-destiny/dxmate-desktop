c2b943a2c92b74e18d90cca9e046bfb34d084267
MSI\destiny <info.dxmate@gmail.com>
2023-12-02 23:05:29 +0900

    Add index.html

---
+++ src/index.js
@@ -0,0 +1,32 @@
+// Modules to control application life and create native browser window
+const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
+
+function createWindow () {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadFile(__dirname + '/index.html');
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
